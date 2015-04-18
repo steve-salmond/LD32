33f7fc610edde22bef059049b983181134b8abf9
Steve Salmond <steve.salmond@smallworlds.com>
2015-04-19 10:59:46 +1200

    Implemented Vacuum gun.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,8 +6,8 @@ public class Gun : MonoBehaviour
     /** Where projectiles are emitted from. */
     public Transform Emitter;
 
-    /** Prefabs to use for when creating projectiles. */
-    public List<GameObject> Projectiles;
+    /** Target transform (used to work around issues with player mirroring). */
+    public Transform Target;
 
     /** Delay range between successive projectiles. */
     public Vector2 DelayRange;
@@ -18,34 +18,120 @@ public class Gun : MonoBehaviour
     /** Angular speed range of projectile. */
     public Vector2 AngularSpeedRange;
 
+    /** Range at which objects get sucked into the emitter. */
+    public float CaptureRange = 1;
 
-    /** Recycling threshold. */
-    public int MaxProjectiles;
+    /** Layer mask for things that can be captured. */
+    public LayerMask CaptureMask;
+
+    /** Range for sucking objects in. */
+    public float SuckRange = 5;
+
+    /** Radius for sucking objects in. */
+    public float SuckRadius = 1;
+
+    /** Sucktion power! */
+    public float SuckForce = 100;
+
+    /** Falloff of suction power with distance. */
+    public AnimationCurve SuckFalloff;
+
+    /** Suck effect. */
+    public GameObject SuckEffect;
+
+    /** Blow effect. */
+    public GameObject BlowEffect;
 
 
     /** Time that next projectile can be fired. */
     private float _nextProjectile;
 
+    /** Emitter direction vector. */
+    private Vector3 _direction;
+
     /** List of previously created projectiles. */
     private readonly Queue<GameObject> _projectiles = new Queue<GameObject>();
-    
+
+    private ParticleSystem[] _suckParticles;
+    private ParticleSystem[] _blowParticles;
+
+    void Start()
+    {
+        _suckParticles = SuckEffect.GetComponentsInChildren<ParticleSystem>();
+        _blowParticles = BlowEffect.GetComponentsInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var blowing = Input.GetButton("Fire1");
+        var sucking = !blowing && Input.GetButton("Fire2");
+
+        if (blowing)
+            Blow();
+        else if (sucking)
+            Suck();
+
+        foreach (var system in _suckParticles)
+            system.enableEmission = sucking;
+        foreach (var system in _blowParticles)
+            system.enableEmission = blowing;
+    }
+
+    void LateUpdate()
+    {
+        SuckEffect.transform.position = Emitter.position;
+        SuckEffect.transform.LookAt(Target.position, Vector3.up);
+        BlowEffect.transform.position = Emitter.position;
+        BlowEffect.transform.LookAt(Target.position, Vector3.up);
+    }
+
+    /** Suck projectiles into the emitter. */
+    void Suck()
+    {
+        var direction = (Emitter.TransformPoint(Vector3.right) - Emitter.position).normalized;
+        var suckers = Physics2D.CircleCastAll(Emitter.position, SuckRadius, direction, SuckRange, CaptureMask);
+        foreach (var sucker in suckers)
+        {
+            var r = sucker.rigidbody;
+            if (r == null)
+                continue;
+
+            var delta = Emitter.position - sucker.transform.position;
+            var d = delta.magnitude;
+
+            if (d < CaptureRange)
+            {
+                // Object is close enough, grab it!
+                var projectile = sucker.collider.gameObject;
+                projectile.SetActive(false);
+                _projectiles.Enqueue(projectile);
+            }
+            else
+            {
+                // Not close enough, suck it towards us.
+                var f = delta.normalized * SuckFalloff.Evaluate(d/SuckRange)*SuckForce;
+                r.AddForce(f);
+            }
+        }
+    }
+
 
     /** Fire a projectile from the emitter. */
-    public void Fire()
+    void Blow()
     {
         // Check if we can fire again yet.
         if (Time.time < _nextProjectile)
             return;
 
-        // Ensure projectile count doesn't get outta control by recycling old ones.
-        if (_projectiles.Count >= MaxProjectiles)
-            Destroy(_projectiles.Dequeue());
+        // Check if player has any projectiles.
+        if (_projectiles.Count <= 0)
+            return;
 
         // Create new projectile.
-        var prefab = Projectiles[Random.Range(0, Projectiles.Count)];
-        var projectile = Instantiate(prefab, Emitter.position, Emitter.rotation) as GameObject;
-        if (projectile == null)
-            return;
+        var projectile = _projectiles.Dequeue();
+        projectile.transform.position = Emitter.position;
+        projectile.transform.rotation = Emitter.rotation;
+        projectile.SetActive(true);
 
         // Give projectile its initial velocity.
         var r = projectile.GetComponent<Rigidbody2D>();
@@ -56,9 +142,6 @@ public class Gun : MonoBehaviour
         var direction = (Emitter.TransformPoint(Vector3.right) - Emitter.position).normalized;
         r.velocity = (direction * speed);
         r.angularVelocity = Random.Range(AngularSpeedRange.x, AngularSpeedRange.y);
-
-        // Add projectile to the recycling queue.
-        _projectiles.Enqueue(projectile);
 
         // Schedule next projectile.
         _nextProjectile = Time.time + Random.Range(DelayRange.x, DelayRange.y);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,15 +19,14 @@ public class PlayerController : Singleton<PlayerController>
     public float MovingDrag = 0;
 
     public float AimOffsetScale = 5;
-    public Gun Gun;
     
     private Camera _camera;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
 
-    private bool _grounded;
-    private bool _facingRight = true;
-    private bool _aimingRight = true;
+    public bool Grounded { get; private set; }
+    public bool FacingRight { get; private set; }
+    public bool AimingRight { get; private set; }
 
     private bool _jump;
 
@@ -46,10 +45,6 @@ public class PlayerController : Singleton<PlayerController>
         // Do this in Update, since it can be missed in FixedUpdate().
         _jump = Input.GetButtonDown("Jump");
         UpdateJump();
-
-        // Handle player firing.
-        if (Input.GetButton("Fire1"))
-            Gun.Fire();
     }
 
     void FixedUpdate()
@@ -67,13 +62,13 @@ public class PlayerController : Singleton<PlayerController>
         var a = p - GroundArea * 0.5f;
         var b = p + GroundArea * 0.5f;
         var hit = Physics2D.OverlapArea(a, b, GroundMask);
-        _grounded = hit != null;
+        Grounded = hit != null;
     }
 
     void UpdateMovement()
     {
         // Get input force scaling factor.
-        var scale = _grounded ? InputForceScale : AirborneForceScale;
+        var scale = Grounded ? InputForceScale : AirborneForceScale;
 
         // Get player's inputs.
         var dx = Input.GetAxisRaw("Horizontal") * scale.x;
@@ -94,18 +89,18 @@ public class PlayerController : Singleton<PlayerController>
 
         // Update player's overall facing.
         if (Mathf.Abs(velocity.x) < 0.25f)
-            _facingRight = _aimingRight;
+            FacingRight = AimingRight;
         else 
-            _facingRight = velocity.x >= 0;
+            FacingRight = velocity.x >= 0;
 
         // Update pelvis scale to match facing.
-        Pelvis.localScale = _facingRight ? Vector3.one : new Vector3(-1, 1, 1);
+        Pelvis.localScale = FacingRight ? Vector3.one : new Vector3(-1, 1, 1);
     }
 
     void UpdateJump()
     {
         // Check if player wishes to jump and is grounded.
-        if (!_jump || !_grounded)
+        if (!_jump || !Grounded)
             return;
 
         // Apply jump force.
@@ -130,10 +125,10 @@ public class PlayerController : Singleton<PlayerController>
         Vector2 pelvis = Pelvis.position;
         var delta = mouse - pelvis;
         var angle = Vector2.Angle(delta, Vector2.up);
-        var flip = _facingRight ? (delta.x >= 0) : (delta.x < 0);
+        var flip = FacingRight ? (delta.x >= 0) : (delta.x < 0);
 
         // Determine if player is aiming to the right or left.
-        _aimingRight = delta.x >= 0;
+        AimingRight = delta.x >= 0;
 
         // Update player's torso to point gun at mouse.
         Torso.localRotation = Quaternion.Euler(0, 0, flip ? 90 - angle : angle - 90);
