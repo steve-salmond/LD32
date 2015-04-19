146a8ee5219026470bf7843186d5913f27300283
Steve Salmond <steve.salmond@smallworlds.com>
2015-04-20 09:23:13 +1200

    Working on level layout.

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -6,6 +6,9 @@ public class SpawnObjects : MonoBehaviour
 
     public Bounds Extents;
 
+    public LayerMask BlockerMask;
+    public float BlockRadius = 0.5f;
+
     public int MinCount;
     public int MaxCount = 10;
 
@@ -17,12 +20,13 @@ public class SpawnObjects : MonoBehaviour
     {
 
         int n = Random.Range(MinCount, MaxCount);
-        for (var i = 0; i < n; i++)
-            Spawn();
+        while (n > 0)
+            if (Spawn())
+                n--;
 
 	}
 
-    void Spawn()
+    bool Spawn()
     {
         var prefab = Prefabs[Random.Range(0, Prefabs.Length)];
 
@@ -34,12 +38,18 @@ public class SpawnObjects : MonoBehaviour
         var z = Random.Range(min.z, max.z);
 
         var p = new Vector3(x, y, z);
+
+        // Check if we can place here.
+        if (Physics2D.OverlapCircle(p, BlockRadius, BlockerMask) != null)
+            return false;
+
         var r = Quaternion.Euler(0, 0, Random.Range(0, 360));
         var go = Instantiate(prefab, p, r) as GameObject;
 
         var s = Random.Range(ScaleRange.x, ScaleRange.y);
         go.transform.localScale = new Vector3(s, s, s);
         go.transform.parent = transform;
+        return true;
     }
 	
 }
